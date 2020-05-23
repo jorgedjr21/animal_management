02e87c7d5c22c7923d32e9e00367ecaeda3abece
Jorge Junior <jorgedjr21@gmail.com>
2020-05-22 21:09:34 -0300

    adds shoulda matchers

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,13 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+$('document').ready(function(){
+  $('.date').mask('00/00/0000 00:00');
+  
+  $(function () {
+    $('[data-toggle="popover"]').popover()
+  });
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
